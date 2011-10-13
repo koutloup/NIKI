c2151893647979e828430eb8f561e9caecebab3c
Petros Koutloubasis <koutloup@gmail.com>
2011-10-13 17:39:41 +0200

    changed license from GPL to LGPL (v3)

--- src/niki/Deck.java
+++ src/niki/Deck.java
@@ -1,16 +1,18 @@
 /*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright 2011 Petros Koutloubasis <koutloup@gmail.com>
  *
- * This program is distributed in the hope that it will be useful,
+ * NIKI is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * NIKI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NIKI. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package niki;
@@ -21,10 +23,6 @@ import java.util.Collections;
 
 import static niki.NikiConstants.*;
 
-/**
- *
- * @author kekse
- */
 public class Deck {
     private ArrayList<Long> deck;
     private ArrayList<Integer> primes;
--- src/niki/Hand.java
+++ src/niki/Hand.java
@@ -1,26 +1,24 @@
 /*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright 2011 Petros Koutloubasis <koutloup@gmail.com>
  *
- * This program is distributed in the hope that it will be useful,
+ * NIKI is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * NIKI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NIKI. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package niki;
 
 import static niki.NikiConstants.*;
 
-/**
- *
- * @author Petros Koutloubasis <koutloup@gmail.com>
- */
 public class Hand {
     public long cards = 0;
     public long key = 1;
